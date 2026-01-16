bf5b65eadf46a02b07216a5f46c0aa4847688a2c
tejtejathanu <tejteja259@gmail.com>
2026-01-16 12:13:31 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,15 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use('*', cors({
+app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://localhost:5174', 
     'http://localhost:5175', 
     'http://localhost:5176',
     'https://emptio-shoping.vercel.app',
-    'https://emptio-shoping-fyopdv69w-tejas-projects-b09c0799.vercel.app',
     'https://emptio-shoping-tozz.vercel.app',
-    'https://emptio-shoping-tozz-jgvrk78np-tejas-projects-b09c0799.vercel.app'
+   
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
